2419bf319fd446f3859e4a8ec01f3b93287fb1fc
Andrés Emmanuel González Sánchez <andres.gonzalez@encora.com>
2024-12-20 14:45:55 -0600

    Fixes the path on the BreakableToy1Application. Adds outOfStock testing.

--- breakable-toy-1-backend/src/main/java/encora/breakable_toy_1/BreakableToy1Application.java
+++ breakable-toy-1-backend/src/main/java/encora/breakable_toy_1/BreakableToy1Application.java
@@ -18,8 +18,7 @@ public class BreakableToy1Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("http://localhost:8080");
-				registry.addMapping("/").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
 			}
 		};
 	}
--- breakable-toy-1-backend/src/test/java/encora/breakable_toy_1/BreakableToy1ApplicationTests.java
+++ breakable-toy-1-backend/src/test/java/encora/breakable_toy_1/BreakableToy1ApplicationTests.java
@@ -1,13 +1,27 @@
 package encora.breakable_toy_1;
 
+import encora.breakable_toy_1.model.Product;
+import encora.breakable_toy_1.repository.ProductRepositoryInMemoryImpl;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class BreakableToy1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void outOfStock() {
+		// Given
+		ProductRepositoryInMemoryImpl repo = new ProductRepositoryInMemoryImpl();
+		repo.create("category", "name", 10.0, LocalDate.now(), 5L);
+
+		// When
+		Product result = repo.outOfStock(1L);
+
+		// Then
+		assertEquals(0L, result.getStock());
 	}
 
 }
