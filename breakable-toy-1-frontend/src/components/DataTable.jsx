59ade29066042fd3efa96bc81890e4c7d4fd7476
Andrés Emmanuel González Sánchez <andres.gonzalez@encora.com>
2024-12-27 14:04:21 -0600

    Changed the DataTable to use pagination from the backend instead of receiving every item.

--- breakable-toy-1-frontend/src/components/DataTable.jsx
+++ breakable-toy-1-frontend/src/components/DataTable.jsx
@@ -12,66 +12,40 @@ const columns = [
   { field: 'actions', headerName: 'Actions', width: 200, sortable: false, align: 'center', headerAlign: 'center', disableColumnMenu: true},
 ];
 
-const paginationModel = { page: 0, pageSize: 10 };
-
 export default function DataTable() {
 
     const [products, setProducts] = React.useState([]);
     const [error, setError] = React.useState(null);
     const [loading, setLoading] = React.useState(true);
-    const [selectedIds, setSelectedIds] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [pageSize, setPageSize] = React.useState(10);
+    const [rowCount, setRowCount] = React.useState(0);
+    const [paginationModel, setPaginationModel] = React.useState({
+        page: 0,
+        pageSize: 10,
+      });
 
      React.useEffect(() => {
          const fetchProducts = async () => {
+             console.log(paginationModel)
+           setLoading(true);
            try {
-             const response = await fetch("http://localhost:9090/", {
+             const response = await fetch(`http://localhost:9090/pagination?page=${paginationModel.page}`, {
                method: "GET",
                headers: { "Content-Type": "application/json" },
              });
              const data = await response.json();
-             setProducts(data);
+             console.log(data)
+             setRowCount(data["page"]["totalElements"]);
+             setProducts(data["_embedded"]["products"]);
            } catch (error) {
              console.error("Error fetching products:", error);
+           } finally {
+            setLoading(false);
            }
          };
          fetchProducts();
-       }, []);
-
-       const handleRowSelectionChange = React.useCallback((selectionModel) => {
-         // Find IDs that were added (checked)
-         const addedIds = selectionModel.filter((id) => !selectedIds.includes(id));
-
-         // Find IDs that were removed (unchecked)
-         const removedIds = selectedIds.filter((id) => !selectionModel.includes(id));
-
-         if (addedIds.length > 0) {
-           fetch(`http://localhost:8080/products/${addedIds[0]}/outofstock`, {
-               method: "POST",
-               headers: { "Content-Type": "application/json" },
-             })
-               .then((response) => {
-                 if (!response.ok) {
-                   throw new Error("Network response was not ok");
-                 }
-                 return response.json();
-               })
-               .then((data) => {
-                 console.log("Product saved:", data);
-               })
-               .catch((error) => {
-                 console.error("Error:", error);
-               });
-         }
-
-         if (removedIds.length > 0) {
-           console.log("Unchecked:", removedIds);
-         }
-
-         // Update the selected IDs state only if it has changed
-         if (JSON.stringify(selectionModel) !== JSON.stringify(selectedIds)) {
-           setSelectedIds(selectionModel);
-         }
-       }, [selectedIds]);
+       }, [paginationModel]);
 
   return (
       <Container maxWidth="xl" >
@@ -79,10 +53,15 @@ export default function DataTable() {
           <DataGrid
             rows={products}
             columns={columns}
-            initialState={{ pagination: { paginationModel } }}
-            pageSizeOptions={[10]}
+            rowCount={rowCount}
+            paginationMode="server"
+            pagination
+            paginationModel = {paginationModel}
+            pageSizeOptions = {[10]}
             checkboxSelection
-            onRowSelectionModelChange={handleRowSelectionChange}
+            loading={loading}
+            //onRowSelectionModelChange={handleRowSelectionChange}
+            onPaginationModelChange={(newModel) => {setPaginationModel(newModel);}}
             sx={{ border: "2px black solid", }}
           />
         </Paper>
